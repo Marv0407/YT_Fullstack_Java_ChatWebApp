4aed9ba6ff687cabba0071fe48b7d7cc79c82aed
Marvin <141830291+Marv0407@users.noreply.github.com>
2025-09-23 19:47:47 +0200

    add connect/disconnect methods

--- src/main/java/com/example/websocket_demo/WebsocketController.java
+++ src/main/java/com/example/websocket_demo/WebsocketController.java
@@ -22,4 +22,18 @@ public class WebsocketController {
         messagingTemplate.convertAndSend("/topic/message", message);
         System.out.println("Sent message to /topic/messages: " + message.getUser() + ": " + message.getMessage());
     }
+
+    @MessageMapping("/connect")
+    public void connectUser(String username) {
+        sessionManager.addUsername(username);
+        sessionManager.broadcastActiveUsernames();
+        System.out.println("User " + username + " connected");
+    }
+
+    @MessageMapping("/disconnect")
+    public void disconnectUser(String username) {
+        sessionManager.removeUsername(username);
+        sessionManager.broadcastActiveUsernames();
+        System.out.println("User " + username + " disconnected");
+    }
 }
